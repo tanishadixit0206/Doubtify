6c899c3473a01b7bd5585d7dbadc038f9dfa8aeb
DeenankSharma <sharmadeenank@gmail.com>
2024-06-21 17:49:29 +0530

    feat : adds add a doubt window

--- frontend/react-demo/src/components/AddSolutionDiv.jsx
+++ frontend/react-demo/src/components/AddSolutionDiv.jsx
@@ -3,21 +3,40 @@ import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-import doubt from '../images/doubt.jpeg'
-import { maths, chemistry, biology, all, physics } from '../subjects'
+import doubt from '../images/doubt.jpeg';
+import { maths, chemistry, biology, all, physics } from '../subjects';
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
-
+import "./AddSolutionDiv.css"
 
 function AddSolutionDiv() {
+    
+    const [hover1, setHover1] = useState(false);
+
+    function ishover1() {
+        setHover1(true);
+    }
+    function isnothover1() {
+        setHover1(false);
+    }
+    const [hover2, setHover2] = useState(false);
+
+    function ishover2() {
+        setHover2(true);
+    }
+    function isnothover2() {
+        setHover2(false);
+    }
     const [options, setOptions] = useState([]);
-    const [value, setValue] = useState("")
+    const [value, setValue] = useState("");
 
     function onChange(event) {
-        console.log(event.target.value)
-        setValue(event.target.value)
+        console.log(event.target.value);
+        setValue(event.target.value);
     }
-    const values = ["All", "Physics", "Chemistry", "Maths", "Biology"]
+
+    const values = ["All", "Physics", "Chemistry", "Maths", "Biology"];
+
     useEffect(() => {
         let options;
         switch (value) {
@@ -28,13 +47,13 @@ function AddSolutionDiv() {
                 options = physics;
                 break;
             case "Chemistry":
-                options = chemistry
+                options = chemistry;
                 break;
             case "Biology":
-                options = biology
+                options = biology;
                 break;
             case "Maths":
-                options = maths
+                options = maths;
                 break;
             default:
                 options = [];
@@ -42,26 +61,88 @@ function AddSolutionDiv() {
         setOptions(options);
     }, [value]);
 
-    return <div className="container col-lg-8">
-        <h1>Add a Doubt!</h1>
-        <div className="img-container">
-            <img src={doubt} alt="image" />
-        </div>
-        <div className="form-container">
-            <Autocomplete
-                options={values}
-                style={{ width: 300 }}
-                renderInput={(params) =>
-                    <TextField onSelect={onChange} {...params} label="Combo box" variant="outlined" />}
-            />
-            <Autocomplete
-                options={options}
-                style={{ width: 300 }}
-                renderInput={(params) =>
-                    <TextField {...params} label="Combo box" variant="outlined" />}
-            />
-        </div>
-    </div>
+    return (
+        <div style={{ borderRadius: "1%", padding: "0", backgroundColor: "rgb(240,240,240)", height: "80vh", overflowY: "scroll", scrollBehavior: "smooth", borderStyle: "solid", borderWidth: "1px", borderColor: "rgb(113, 44, 249)", color: "rgb(113, 44, 249)" }} className="container d-flex flex-column justify-content-start align-items-center col-lg-9 col-md-7 col-sm-6 col-xs-10 my-3 pb-3">
+            <div style={{ width: "100%", textAlign: "center", backgroundColor: "rgb(113, 44, 249)" }} className="mb-3">
+                <h1 style={{ color: "white" }} className="add-solution-heading my-3 ">Add a Doubt</h1>
+            </div>
 
+            <div class="dropzone d-block">
+                <label for="files" class="dropzone-container">
+                    <div class="file-icon"><i class="fa-solid fa-file-circle-plus text-primary"></i></div>
+                    <div class="text-center pt-3 px-5">
+                        <p class="w-80 h5 text-dark fw-bold">To add a doubt</p>
+                        <p class="w-80 h5 text-dark fw-bold">Drag your documents, photos or videos here to start uploading.</p>
+                        <div class="hr-sect">or</div>
+                        <button style={!hover1 ? {
+                            color: "white",
+                            backgroundColor: "rgb(113, 44, 249)",
+                            borderColor: "rgb(113, 44, 249)",
+                            fontFamily: "Poppins",
+                            fontWeight: "700",
+                            fontStyle: "normal",
+                            fontSize: "1.1rem"
+                        } : {
+                            color: "rgb(113, 44, 249)",
+                            backgroundColor: "white",
+                            borderColor: "rgb(113, 44, 249)",
+                            fontFamily: "Poppins",
+                            fontWeight: "700",
+                            fontStyle: "normal",
+                            fontSize: "1.1rem"
+                        }} class="btn btn-primary mb-2" onMouseEnter={ishover1} onMouseLeave={isnothover1}>Browse Files</button>
+                    </div>
+                </label>
+                <input id="files" name="files[]" type="file" class="file-input" />
+            </div>
+            <div className="form-container">
+                <Autocomplete
+                    className="my-3"
+                    options={values}
+                    style={{ width: "60vw" }}
+                    renderInput={(params) => (
+                        <TextField onSelect={onChange} {...params} label="Subject" variant="outlined" />
+                    )}
+                />
+                <Autocomplete
+                    className="my-3"
+                    options={options}
+                    style={{ width: "60vw" }}
+                    renderInput={(params) => (
+                        <TextField {...params} label="Topic" variant="outlined" />
+                    )}
+                />
+            </div>
+            <div class="dropzone d-block">
+                <label for="files" class="dropzone-container">
+                    <div class="file-icon"><i class="fa-solid fa-file-circle-plus text-primary"></i></div>
+                    <div class="text-center pt-3 px-5">
+                        <p class="w-80 h5 text-dark fw-bold">To add a solution</p>
+                        <p class="w-80 h5 text-dark fw-bold">Drag your documents, photos or videos here to start uploading.</p>
+                        <div class="hr-sect">or</div>
+                        <button style={!hover2 ? {
+                            color: "white",
+                            backgroundColor: "rgb(113, 44, 249)",
+                            borderColor: "rgb(113, 44, 249)",
+                            fontFamily: "Poppins",
+                            fontWeight: "700",
+                            fontStyle: "normal",
+                            fontSize: "1.1rem"
+                        } : {
+                            color: "rgb(113, 44, 249)",
+                            backgroundColor: "white",
+                            borderColor: "rgb(113, 44, 249)",
+                            fontFamily: "Poppins",
+                            fontWeight: "700",
+                            fontStyle: "normal",
+                            fontSize: "1.1rem"
+                        }} class="btn btn-primary mb-2" onMouseEnter={ishover2} onMouseLeave={isnothover2}>Browse Files</button>
+                    </div>
+                </label>
+                <input id="files" name="files[]" type="file" class="file-input" />
+            </div>
+        </div>
+    );
 }
-export default AddSolutionDiv
+
+export default AddSolutionDiv;
