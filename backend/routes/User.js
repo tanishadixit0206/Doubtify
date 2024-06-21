1691f7ff39d2d29b594ae20dfce7d49712029f3b
Jayesh_Pandit <jayesh.pandit@iiitb.ac.in>
2024-06-21 09:46:35 +0530

    uploading files to db

--- backend/routes/User.js
+++ backend/routes/User.js
@@ -1,10 +1,30 @@
 import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+import methodOverride from "method-override";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 import {
   justGet,
   uploadHere,
   uploadImage,
 } from "../controller/userController.js";
+
 const router = express();
+
+//middleware
+import bodyParser from "body-parser";
+
+router.use((req, res, next) => {
+  const app = req.app; // Get the main app instance
+  app.set("view engine", "ejs");
+  app.set("views", path.join(__dirname, "../views"));
+  app.use(bodyParser.json);
+  app.use(methodOverride("_method"));
+  next();
+});
 
 router.get("/", justGet);
 router.get("/upload-image", uploadHere);
